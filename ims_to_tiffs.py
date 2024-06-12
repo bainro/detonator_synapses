50c12c3353ba35b5852e27e004070913a18e2318
Robert Bain <31903812+bainro@users.noreply.github.com>
2024-06-11 22:35:35 -0700

    Update ims_to_tiffs.py

--- ims_to_tiffs.py
+++ ims_to_tiffs.py
@@ -1,20 +1,16 @@
+import os
 import tifffile
 from imaris_ims_file_reader.ims import ims
 
-results_dir = os.path.join(os.getcwd(), r"/tiffs/")
+results_dir = os.path.join(os.getcwd(), r"tiffs/")
 os.makedirs(results_dir, exist_ok=True)
 
 ims_files = os.listdir(os.getcwd())
 ims_files = [os.path.join(os.getcwd(), f) for f in ims_files]
 
 for ims_file in ims_files:
-    root_name = ims_file.split(r'/')[-1][:-4]
-    ims_data = ims(ims_file)
-    converted_file = os.path.join(results_dir, root_name)
-    tifffile.imwrite(converted_file, ims_data[0,::2,0,:,:])
-
-# psd_ch = ims_data[0,0,0,:,:].copy()
-# syn_ch = ims_data[0,1,0,:,:].copy()
-# will keep them separate and just crop one because thresholding will functionally crop the other :)
-# tifffile.imwrite(f'{r"/tmp/"}{root_name}_CH1.tiff', psd_ch)
-# tifffile.imwrite(f'{r"/tmp/"}{root_name}_CH2.tiff', syn_ch)
+    if os.path.isfile(ims_file) and ims_file[-4:] == '.ims':
+        root_name = ims_file.split(r'/')[-1][:-4]
+        ims_data = ims(ims_file)
+        converted_file = os.path.join(results_dir, root_name + ".tiff")
+        tifffile.imwrite(converted_file, ims_data[0,:,0,:,:])
