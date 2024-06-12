73fd126c29da54937048e2a7cfcddc4e76b7cf68
Robert Bain <31903812+bainro@users.noreply.github.com>
2024-06-11 22:20:54 -0700

    Update ims_to_tiffs.py

--- ims_to_tiffs.py
+++ ims_to_tiffs.py
@@ -1,16 +1,20 @@
-# if we do use this, make sure to add the cell counts as prefixes too!
-
 import tifffile
 from imaris_ims_file_reader.ims import ims
 
-file_1 = r'/home/rbain/Desktop/ELA_soma_dets_imaris/right_CA3_6L_tiled_first_z_snap_t_right_CA3_6L_tiled_first_z_snap.ims'
-file_2 = r'/home/rbain/Desktop/ELA_soma_dets_imaris/right_CA3_10R_tiled_first_z_snap_t_right_CA3_10R_tiled_first_z_snap.ims'
-for ims_file in [file_1, file_2]:
+results_dir = os.path.join(os.getcwd(), r"/tiffs/")
+os.makedirs(results_dir, exist_ok=True)
+
+ims_files = os.listdir(os.getcwd())
+ims_files = [os.path.join(os.getcwd(), f) for f in ims_files]
+
+for ims_file in ims_files:
     root_name = ims_file.split(r'/')[-1][:-4]
     ims_data = ims(ims_file)
-    psd_ch = ims_data[0,0,0,:,:].copy()
-    syn_ch = ims_data[0,1,0,:,:].copy()
-    # will keep them separate and just crop one because thresholding will functionally crop the other :)
-    tifffile.imwrite(f'{r"/tmp/"}{root_name}_CH1.tiff', psd_ch)
-    tifffile.imwrite(f'{r"/tmp/"}{root_name}_CH2.tiff', syn_ch)
+    converted_file = os.path.join(results_dir, root_name)
+    tifffile.imwrite(converted_file, ims_data[0,::2,0,:,:])
 
+# psd_ch = ims_data[0,0,0,:,:].copy()
+# syn_ch = ims_data[0,1,0,:,:].copy()
+# will keep them separate and just crop one because thresholding will functionally crop the other :)
+# tifffile.imwrite(f'{r"/tmp/"}{root_name}_CH1.tiff', psd_ch)
+# tifffile.imwrite(f'{r"/tmp/"}{root_name}_CH2.tiff', syn_ch)
